320de5418f89501b6c8acdb425ce0f347fa7a0c4
Thomas Maier <tm@konzept-54.de>
2023-11-09 12:32:52 +0100

    refactor scripts, added trigger curl

--- src/get-samples.js
+++ src/get-samples.js
@@ -5,17 +5,18 @@ const LOGGER = require("./logger");
 
 // Variables
 const Logger = new LOGGER("Get-Samples");
-const SEARCH = process.argv.slice(2); // the search query
+const SEARCH = process.argv[2]; // the search query
 const ATTRIBUTE = "data-mp3";
-
-console.log(process.env);
 
 // Functions
 const downloadMp3 = async function (url, fileName) {
-  return CP.execSync(`curl -o ${fileName}  '${url}'`);
+  CP.execSync(`curl -o ${fileName}  '${url}'`);
+  return fileName;
 };
 
 const getSounds = async (url) => {
+  const mp3Files = [];
+
   try {
     const response = await AXIOS.get(url);
     const queryHtml = CHEERIO.load(response.data);
@@ -25,17 +26,18 @@ const getSounds = async (url) => {
     );
     Logger.log(url, " : ", urls);
 
-    // for (let [index, mp3Url] of urls.entries()) {
-    //   downloadMp3(
-    //     mp3Url,
-    //     `/Users/thomasmaier/Documents/Repos/song-creation-pipeline/files/${index}-${SEARCH}.mp3`
-    //   );
-    // }
+    for (let [index, mp3Url] of urls.entries()) {
+      const mp3File = downloadMp3(mp3Url, `${index}-${SEARCH}.mp3`);
 
-    return true;
+      if (mp3File) {
+        mp3Files.push(mp3File);
+      }
+    }
+
+    return mp3Files;
   } catch (error) {
     console.error(error);
-    return false;
+    return mp3Files;
   }
 };
 
@@ -43,11 +45,11 @@ const getSounds = async (url) => {
 (async () => {
   Logger.log("--- Start ---");
 
-  // TODO: MKDIR in Drive Inbox
-
-  await getSounds(
+  const mp3Files = await getSounds(
     `https://freesound.org/search/?q=${SEARCH}&f=license%3A%22creative+commons+0%22+duration%3A%5B0+TO+20%5D&w=&tm=0&s=Automatic+by+relevance&advanced=1&g=&only_p=&cm=0&page=1#sound`
   );
+
+  // TODO: Upload to Drive
 
   Logger.log("--- End ---");
 })();
