cec4274ed8dfe74a68abc367e93b1a4ba9a32faa
Thomas Maier <tm@konzept-54.de>
2023-11-21 16:51:01 +0100

    cleanup

--- curls.sh
+++ curls.sh
@@ -5,3 +5,11 @@ curl -L -X POST \
   -H "Authorization: Bearer TOKEN" \
   https://api.github.com/repos/tinkeringaround/song-creation-pipeline/actions/workflows/75556103/dispatches \
   -d '{"ref":"main","inputs":{"search_query": "SEARCH"}}'
+
+# Trigger Get-Word-Suggestions Workflow
+curl -L -X POST \ 
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Authorization: Bearer TOKEN" \
+  https://api.github.com/repos/tinkeringaround/song-creation-pipeline/actions/workflows/76864426/dispatches \
+  -d '{"ref":"main","inputs":{"search_query": "SEARCH"}}'
